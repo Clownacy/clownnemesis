addbe76286c767737b0cb7f1a25ecfc7ac6e23a1
Clownacy <Clownacy@users.noreply.github.com>
2024-02-21 23:01:44 +0000

    Make libraries linkable from C++.

--- compress.h
+++ compress.h
@@ -3,7 +3,15 @@
 
 #include "common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Returns 0 on error. */
 int ClownNemesis_Compress(int accurate, ClownNemesis_InputCallback read_byte, const void *read_byte_user_data, ClownNemesis_OutputCallback write_byte, const void *write_byte_user_data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* HEADER_GUARD_3806BB18_BC2F_47C7_B9EF_8826358CB908 */
